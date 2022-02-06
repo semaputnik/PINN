5fda5aea82758e2ffc053c524e159cd64bca7a08
Semaputnik <semyon.putnikov@gmail.com>
2022-02-06 18:28:31 +0300

    update result plot

--- First_Phase/postprocess.py
+++ First_Phase/postprocess.py
@@ -14,6 +14,11 @@ if __name__ == "__main__":
 
     source_data: DataFrame = pd.read_csv(source_data_path)
 
+    r2_S = round(m.r2_score(source_data["S"], predicted_data["S"]), 4)
+    r2_E = round(m.r2_score(source_data["E"], predicted_data["E"]), 4)
+    r2_I = round(m.r2_score(source_data["I"], predicted_data["I"]), 4)
+    r2_R = round(m.r2_score(source_data["R"], predicted_data["R"]), 4)
+
     fig = plt.figure(figsize=(12, 12))
     ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
     ax.set_facecolor('xkcd:white')
@@ -21,16 +26,19 @@ if __name__ == "__main__":
     #ax.plot(source_data.index, source_data["S"], 'pink', alpha=0.5, lw=2, label='Susceptible')
     #ax.plot(source_data.index, predicted_data["S"], 'red', alpha=0.9, lw=2, label='Susceptible Prediction', linestyle='dashed')
 
-    ax.plot(source_data.index, source_data["E"], 'violet', alpha=0.5, lw=2, label='Exposed')
-    ax.plot(source_data.index, predicted_data["E"], 'dodgerblue', alpha=0.9, lw=2, label='Exposed Prediction', linestyle='dashed')
+    ax.plot(source_data.index, source_data["E"], 'red', alpha=0.5, lw=2, label='Exposed')
+    ax.plot(source_data.index, predicted_data["E"], 'coral', alpha=0.9, lw=2,
+            label='Exposed Prediction (R^2 : {})'.format(r2_E), linestyle='dashed')
 
     ax.plot(source_data.index, source_data["I"], 'darkgreen', alpha=0.5, lw=2, label='Infected')
-    ax.plot(source_data.index, predicted_data["I"], 'green', alpha=0.9, lw=2, label='Infected Prediction', linestyle='dashed')
+    ax.plot(source_data.index, predicted_data["I"], 'green', alpha=0.9, lw=2,
+            label='Infected Prediction (R^2 : {})'.format(r2_I), linestyle='dashed')
 
-    ax.plot(source_data.index, source_data["R"], 'blue', alpha=0.5, lw=2, label='Recovered')
-    ax.plot(source_data.index, predicted_data["R"], 'teal', alpha=0.9, lw=2, label='Recovered Prediction', linestyle='dashed')
+    ax.plot(source_data.index, source_data["R"], 'darkblue', alpha=0.5, lw=2, label='Recovered')
+    ax.plot(source_data.index, predicted_data["R"], 'blue', alpha=0.9, lw=2,
+            label='Recovered Prediction (R^2 : {})'.format(r2_R), linestyle='dashed')
 
-    ax.set_xlabel('Time /days', fontsize=20)
+    ax.set_xlabel('Day', fontsize=20)
     ax.set_ylabel('Number', fontsize=20)
     ax.yaxis.set_tick_params(length=0)
     ax.xaxis.set_tick_params(length=0)
@@ -47,8 +55,8 @@ if __name__ == "__main__":
     print('MSE(I): ', round(m.mean_squared_error(source_data["I"], predicted_data["I"]), 4))
     print('MSE(R): ', round(m.mean_squared_error(source_data["R"], predicted_data["R"]), 4))
 
-    print('R^2(S): ', round(m.r2_score(source_data["S"], predicted_data["S"]), 4))
-    print('R^2(E): ', round(m.r2_score(source_data["E"], predicted_data["E"]), 4))
-    print('R^2(I): ', round(m.r2_score(source_data["I"], predicted_data["I"]), 4))
-    print('R^2(R): {}'.format(round(m.r2_score(source_data["R"], predicted_data["R"]), 4)))
+    print('R^2(S): ', r2_S)
+    print('R^2(E): ', r2_E)
+    print('R^2(I): ', r2_I)
+    print('R^2(R): ', r2_R)
 
